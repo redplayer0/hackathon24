827d12269096d2e28e4ace53a1a392565d213d33
karak <karakoulaj@gmail.com>
2024-05-16 23:10:04 +0300

    transactionhistory

--- web/src/app/main-application/transactionhistory/transactionhistory.component.ts
+++ web/src/app/main-application/transactionhistory/transactionhistory.component.ts
@@ -2,12 +2,7 @@ import { Component } from '@angular/core';
 import { UserService } from '../../services/userService';
 import { IUser, IUserLogin, UserLogIn,TransactionOfUser } from '../../models/models';
 
-interface Transaction {
-  shopName: string;
-  amount: number;
-  date: string;
-  status: string;
-}
+
 @Component({
   selector: 'app-transactionhistory',
   templateUrl: './transactionhistory.component.html',
